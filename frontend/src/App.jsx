f007587fed7c0eca16e6564959db913e97b5a3ba
Rudra Gaur <rudrag9870@gmail.com>
2026-05-18 00:22:43 +0530

    onboarding page completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import OnboardingPage from "./pages/OnboardingPage";
@@ -8,20 +8,63 @@ import ChatPage from "./pages/ChatPage";
 import CallPage from "./pages/CallPage";
 import HomePage from "./pages/HomePage";
 import toast, { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios";
+import PageLoader from "./components/PageLoader";
+import useAuthUser from "./hooks/useAuthUser";
 
 const App = () => {
+  const { isLoading, authUser } = useAuthUser();
+
+  const isAuthenticated = Boolean(authUser);
+  const isOnboarded = authUser?.onboarded;
+
+  if (isLoading) {
+    return <PageLoader />;
+  }
   return (
     <div className=" h-screen" data-theme="light">
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/notification" element={<NotificationPage />} />
-        <Route path="/call" element={<CallPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route
+          path="/"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <HomePage />
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
+        />
+        <Route
+          path="/signup"
+          element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/notification"
+          element={
+            isAuthenticated ? <NotificationPage /> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path="/call"
+          element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/chat"
+          element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/onboarding"
+          element={
+            isAuthenticated ? <OnboardingPage /> : <Navigate to="/login" />
+          }
+        />
       </Routes>
-      <Toaster/>
+      <Toaster />
     </div>
   );
 };
---
+++ frontend/src/hooks/useAuthUser.js
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query'
+import { getAuthUser } from '../lib/api'
+
+const useAuthUser = () => {
+    const { data, isLoading } = useQuery({
+        queryKey: ['authUser'],
+        queryFn: getAuthUser,
+        retry: false,
+    })
+
+    return {
+        loading: isLoading,
+        authUser: data?.user,
+    }
+}
+
+export default useAuthUser
